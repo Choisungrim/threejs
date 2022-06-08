6e314298f8e606b3bf735a96a147481acac62b62
SungRim <wowilos@naver.com>
2022-06-08 16:38:27 +0900

    자동회전

--- 03-basic.js
+++ 03-basic.js
@@ -5,21 +5,31 @@ scene.background = new THREE.Color(0x004fff);
 
 // 카메라
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/ window.innerHeight, 0.1, 1000);
-
+camera.position.z=3;
 // 캔버스
 const canvas = document.querySelector('#webgl');
 
 // 렌더링
 const renderer = new THREE.WebGLRenderer({canvas});
+renderer.setSize(window.innerWidth, window.innerHeight);
 
-//document.body.appendChild(renderer.domElement);
-//renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+
+// 매쉬
+const geometry = new THREE.BoxGeometry(0.5,0.5,0.5);
+const material = new THREE.MeshStandardMaterial({
+  color : 0x999999
+});
+const cube = new THREE.Mesh(geometry,material)
+scene.add(cube);
+
 
 function render(time) {
     time *= 0.001;  // convert time to seconds
     
-    //cube.rotation.x = time;
-    //cube.rotation.y = time;
+    cube.rotation.x = time;
+    cube.rotation.y = time;
    
     renderer.render(scene, camera);
    
